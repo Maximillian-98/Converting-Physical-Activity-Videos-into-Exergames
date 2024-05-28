623288833334af85a10cb30842c8b6bd0f315583
Maximillian Atherton <max@a319.co.uk>
2024-05-28 20:44:59 +0100

    Update Pose.py

--- Pose.py
+++ Pose.py
@@ -21,11 +21,28 @@ class PoselandmarkdetectionVIDEO:
 
 # Initialise video capture
         self.cap = cv2.VideoCapture(video_path)
-        if not self.cap.isOpened():
+        if not self.cap.isOpened(): #Checks if it opens properly
             raise ValueError("Error opening video file")
         
 # Video Properties
         self.fps = self.cap.get(cv2.CAP_PROP_FPS) #Used for timestamping each frame
+
+    
+# Processes the video and creates and maps the landmarks
+    def process_video(self):
+        frame_index = 0
+        success, frame = self.cap.read()
+        while success:
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+            timestamp_ms = (frame_index / self.fps) * 1000
+            pose_landmarks = self.landmarker.detect_for_video(mp_image, timestamp_ms)
+            
+            # Add here the landmarker stuff to output the object
+            
+            # Read the next frame
+            success, frame = self.cap.read()
+            frame_index += 1
 
 # The __enter__ method returns the landmarker instance, allowing the class to be used with a with statement.
     def __enter__(self):
@@ -33,5 +50,7 @@ class PoselandmarkdetectionVIDEO:
 # The __exit__ method ensures the landmarker is properly closed when exiting the with block.
     def __exit__(self, exc_type, exc_value, traceback):
         self.landmarker.close()
+        self.cap.release()
 
 
+# Need to finish, add poselanmarkerresult to get the returned object 
