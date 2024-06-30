6c8584837896ba96a315eca34fdee5175079de89
Maximillian Atherton <max@a319.co.uk>
2024-06-30 19:19:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,49 @@ class videoPose:
         self.cap.release()
         cv2.destroyAllWindows()
 
+class livePose:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_pose = mp.solutions.pose
+
+        # Setup mediapipe instance
+        self.pose = self.mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+    def drawPose(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+        
+            # Make detection
+            results = self.pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Render detections
+            self.mp_drawing.draw_landmarks(image, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS,
+                                    self.mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    self.mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )               
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
 video_path = r'C:\Users\max\Documents\GitHub\Converting-Physical-Activity-Videos-into-Exergames\Exercise Videos\PushupsTop.mp4'
 
 video_landmarker = videoPose(video_path)
-video_landmarker.drawPose()
+video_landmarker.drawPose
+
+live_landmarker = livePose()
+live_landmarker.drawPose()
