9455a6a45311d671294e67848697a78c5460f216
Maximillian Atherton <max@a319.co.uk>
2024-07-19 20:54:48 +0100

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 import cv2
 
-class UI:
-    def __init__(self, video_path):
+class MainFrame:
+    def __init__(self, root, video_path):
+        self.root = root
         self.video_path = video_path
+
+
+class ExvidFrame:
+    def __init__(self, root, videos):
+        self.root = root
+
+class WorkoutFrame:
+    def __init__(self, root, videos):
+        self.root = root
