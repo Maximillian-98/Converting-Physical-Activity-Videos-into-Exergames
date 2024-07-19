151488cc501a9d2f6dad7f03007762384b57ca46
Maximillian Atherton <max@a319.co.uk>
2024-07-19 23:44:44 +0100

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import cv2
 
 class MainFrame:
@@ -10,8 +11,22 @@ class MainFrame:
         self.canvas.pack(anchor=tk.CENTER, expand=True)
 
         # Scrollable frames for videos and workout
-        self.exVidFrame = tk.Frame(self.canvas)
-        self.exVidFrame.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+        self.exVidCanvas = tk.Canvas(self.canvas, bg='lightblue')
+        self.exVidScrollbar = ttk.Scrollbar(self.canvas, orient="vertical", command=self.exVidCanvas.yview)
+        self.exVidFrame = ttk.Frame(self.exVidCanvas)
+
+        self.exVidFrame.bind(
+            "<Configure>",
+            lambda e: self.exVidCanvas.configure(
+                scrollregion=self.exVidCanvas.bbox("all")
+            )
+        )
+
+        self.exVidCanvas.create_window((0, 0), window=self.exVidFrame, anchor="nw")
+        self.exVidCanvas.configure(yscrollcommand=self.exVidScrollbar.set)
+
+        self.exVidCanvas.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+        self.exVidScrollbar.place(relx=0.5, rely=0, relheight=1, anchor='ne')
 
 
         self.workoutFrame = tk.Frame(self.canvas)
