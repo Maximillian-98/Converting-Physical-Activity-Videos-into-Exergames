10c1bd1e4fe639cafa70e1e11eefc8e9c9eb0492
Maximillian Atherton <max@a319.co.uk>
2024-08-02 20:58:46 +0100

    Update UI.py

--- UI.py
+++ UI.py
@@ -217,25 +217,13 @@ class PlayFrame:
         self.canvas = tk.Canvas(self.root, width=1000, height=800, bg='white')
         self.canvas.pack(anchor=tk.CENTER, expand=True)
 
-        # Canvas for video and break text
-        self.vidCanvas = tk.Canvas(self.canvas, bg='lightblue')
-        self.vidFrame = ttk.Frame(self.vidCanvas)
-        self.vidCanvas.create_window((0, 0), window=self.vidFrame, anchor="nw")
-        self.vidCanvas.place(relx=0, rely=0, relwidth=1, relheight=0.5)
-
-        self.breakText = tk.Label(self.vidCanvas, text="00:00")
+        self.breakText = tk.Label(self.canvas, text="00:00")
         self.breakText.place(relx=0.45, rely=0.2, relwidth=0.1, relheight=0.1)
 
-        self.backButton = tk.Button(self.vidCanvas, text="Back", command=self.back)
+        self.backButton = tk.Button(self.canvas, text="Back", command=self.back)
         self.backButton.place(relx=0.45, rely=0.4, relwidth=0.1, relheight=0.1)
 
-        # Canvas for live feed
-        self.liveFeedCanvas = tk.Canvas(self.canvas, bg='lightgreen')
-        self.liveFeedFrame = ttk.Frame(self.liveFeedCanvas)
-        self.liveFeedCanvas.create_window((0, 0), window=self.liveFeedFrame, anchor="nw")
-        self.liveFeedCanvas.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
-
-        self.playButton = tk.Button(self.vidCanvas, text="Play", command=self.playWorkout(self.break_time, self.liveFeedCanvas))
+        self.playButton = tk.Button(self.canvas, text="Play", command=self.playWorkout(self.break_time, self.canvas))
         self.playButton.place(relx=0.45, rely=0.3, relwidth=0.1, relheight=0.1)
 
 
