3d30b01c777a25eddabead1b74bddc81b720336b
Maximillian Atherton <max@a319.co.uk>
2024-08-12 22:18:13 +0100

    Update UI.py

--- UI.py
+++ UI.py
@@ -362,7 +362,7 @@ class PlayFrame:
             minutes, seconds = divmod(time_sec, 60)
             timer_str = f"{minutes:02}:{seconds:02}"
             cv2.putText(vid_frame, timer_str, (self.cvWidth // 2 - 50, self.cvHeight // 2), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 255), 4, cv2.LINE_AA)
-            cv2.putText(vid_frame, points, (self.cvWidth // 2 - 50, self.cvHeight // 2), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 255), 4, cv2.LINE_AA)
+            cv2.putText(vid_frame, points, (self.cvWidth // 2 - 50, self.cvHeight // 2 - 20), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 255), 4, cv2.LINE_AA)
 
             # Combine the frames
             combined_frame = cv2.vconcat([cap_frame, vid_frame])
